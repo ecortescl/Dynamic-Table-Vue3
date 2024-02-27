0f33d28907c4e453cf3f9d7a85bef81233a9635c
Esteban Cortés R <70493570+ecortescl@users.noreply.github.com>
2024-02-27 16:30:24 -0300

    :bug: package and vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue'; // Importa el plugin vue
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()], // Usa el plugin vue
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/entry.js'), // Ajusta a tu archivo de entrada
+      name: 'DynamicTableVue3',
+      fileName: (format) => `dynamic-table-vue3.${format}.js`
+    },
+    rollupOptions: {
+      // Asegúrate de externalizar Vue y otras dependencias que no deban ser empaquetadas
+      external: ['vue'],
+      output: {
+        // Proporciona nombres de globals para las dependencias externas, si es necesario, para formatos UMD/IIFE
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 });
